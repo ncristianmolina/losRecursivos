d4e07aaf0f496284e4155c01b2a2e031d0fca0da
LuchiHamade <luciana.b.hamade@gmail.com>
2025-06-24 23:04:28 -0300

    mock.h

--- mock.h
+++ mock.h
@@ -4,11 +4,6 @@
 
 
 
-#define NUM_JUGADORES_ALEATORIOS 2000
-#define NUM_JUGADORES_CARGADOS 0
-#define TOTAL_JUGADORES (NUM_JUGADORES_CARGADOS + NUM_JUGADORES_ALEATORIOS)
-#define jugadoresPrecargados[NUM_JUGADORES_PRECARGADOS];
-
 #define AR_JUGADORES "jugadores.dat"
 
 void generarContrasena(char *destino, int longitud);
@@ -17,7 +12,8 @@ void generarJugadorAleatorio(stJugador *jugador, int idJugador);
 void guardarJugadoresEnArchivoBinario(const char *nombreArchivo, stJugador *jugadores, int cantidad);
 void leerJugadoresDesdeArchivo(const char *nombreArchivo);
 void leerJugadoresDesdeArchivo(const char *nombreArchivo);
-
+int obtenerUltimoIDPlayer();
+void generarYGuardarJugadores(int cantidad);
 
 
 #endif // MOCK_H_INCLUDED

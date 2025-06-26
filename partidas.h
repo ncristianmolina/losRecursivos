d3e61f3516d98e9998345baf6380982e09c40437
LuchiHamade <luciana.b.hamade@gmail.com>
2025-06-26 20:11:10 -0300

    MISIÓN CUMPLIDA SOLDADO, REGRESA A CASA

--- partidas.h
+++ partidas.h
@@ -13,7 +13,6 @@ typedef struct {
  void generarPartidaAleatoria(stPartida *partida, int idPartida);
  void guardarPartidasEnArchivo(const char *nombreArchivo, stPartida *partida, int cantidad);
  void leerPartidasDesdeArchivo(const char *nombreArchivo) ;
- void imprimirPartidas(stPartida partida);
  int calcularPuntos(int resultado);
  void generarYGuardarPartidas(int cantidad);
  int obtenerUltimoIDPartida();
